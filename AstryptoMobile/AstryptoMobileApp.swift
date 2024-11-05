0bebd4cc2dfced7e53fdf612b92a49d261614d28
sstonn <sstonn2501@gmail.com>
2024-11-05 08:56:40 +0700

    Initial Commit

---
+++ AstryptoMobile/AstryptoMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstryptoMobileApp.swift
+//  AstryptoMobile
+//
+//  Created by sstonn on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AstryptoMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
